c19c34ab109882508222544a3fbaa3a3347610cf
Hazel Tan <e0424860@u.nus.edu>
2022-06-08 17:25:48 +0800

    add exercise 3.8: phonebook backend step8

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token("data", function (request, response) {
+  return JSON.stringify(request.body);
+});
+
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
