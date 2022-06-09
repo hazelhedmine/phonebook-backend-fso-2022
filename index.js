6b451e32cf1baa154ab752afff010fc444b237cb
Hazel Tan <e0424860@u.nus.edu>
2022-06-09 12:23:56 +0800

    add exercise 3.9: phonebook backend step9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("data", function (request, response) {
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
+app.use(cors());
 
 let persons = [
   {
