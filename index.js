f73cf66624723ba21565d2b93f50bb5a4d293e0b
Hazel Tan <e0424860@u.nus.edu>
2022-06-09 13:24:06 +0800

    add exercise 3.11: phonebook full stack

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 app.use(cors());
-// app.use(express.static("build"));
+app.use(express.static("build"));
 
 let persons = [
   {
