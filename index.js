0a9e4ac1db3d78b7634bedb4c61e759fb7594a4a
Hazel Tan <e0424860@u.nus.edu>
2022-06-10 17:44:24 +0800

    add exercise 3.16: phonebook database step4

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ morgan.token("data", function (request, response) {
   return JSON.stringify(request.body);
 });
 
+app.use(express.static("build"));
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 app.use(cors());
-app.use(express.static("build"));
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -22,18 +22,25 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id).then((result) => {
-    // successfully deleting returns 204 no content
-    response.status(204).end();
-  });
-  // .catch(error => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      // successfully deleting returns 204 no content
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -62,6 +69,28 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+// no routes or middleware will be called after the response
+// has been sent by unknown endpoint middleware
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// handler of requests with result to errors
+app.use(errorHandler);
 
 // ------------------- before mongoDB
 // let persons = [
